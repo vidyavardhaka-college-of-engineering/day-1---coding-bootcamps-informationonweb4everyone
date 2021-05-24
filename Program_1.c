70c576abc4f09892b33a463fcd7bec8f10ca0998
Badhri Narayanan <>
2021-05-24 12:18:06 +0000

    Written C programming statements for printing my name

--- Program_1.c
+++ Program_1.c
@@ -1,1 +1,7 @@
 //Write a C Program to Display your Name
+#include<stdio.h>
+int main(void)
+{
+  printf("My name is Badhri");
+  printf("I am studying in the department of computer Science in VVCE\n");
+}
